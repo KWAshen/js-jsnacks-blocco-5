f31fe0f284d6056a006a075be7df5a096b770718
KWAshen <ashen.maliaha@gmail.com>
2025-07-23 19:13:15 +0200

    snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,4 +1,8 @@
-const nums = [2, 8, 4, 7, 12, 87];
+const nums = [2, 8, 4, 7, 12, 87]; //o cancellare il 7 e 87 dalla lista
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const evenNums = nums.filter((num) => num % 2 === 0);
+  // return evenNums;
+  console.log(evenNums);
