6d848689d382b4f935dd13b003b132ca9624152d
KWAshen <ashen.maliaha@gmail.com>
2025-07-23 19:07:19 +0200

    snack-4

--- snack-4/script.js
+++ snack-4/script.js
@@ -7,3 +7,10 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const author = posts.map((post) =>{
+  return post.author;
+})
+
+
+  console.log(author)
